e1ef8ccc30dac88ffb3d1096b16fefb819033ba3
Richelle Burke <burkeri@alumni.upenn.edu>
2019-04-19 11:29:42 -0400

    connected to db w/ mongoose

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 // dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+
+// server port
+const PORT = process.env.PORT || 3001;
+
+// mongodb setup
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/codingChallenge";
 
 // start express app
 const app = express();
 
-// server - port
-const PORT = process.env.PORT || 3001;
+// db connection
+mongoose
+  .connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("db connected...");
+  })
+  .catch(err => console.log(err));
 
 // start server
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
 
 
 
@@ -35,12 +47,8 @@ app.listen(PORT, function() {
 //   app.use(express.static("client/build"));
 // }
 
-
 // // Send every request to the React app
 // // Define any API routes before this runs
 // app.get("*", function(req, res) {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
-
-
-

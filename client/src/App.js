27732345671db7b840a9c5a8ab30eb05e86675ad
Richelle Burke <burkeri@alumni.upenn.edu>
2019-04-20 19:47:23 -0400

    data received on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,33 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// import files
+import API from "../src/utils/API";
+
 // pages
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 
 class App extends Component {
+
+  state = {
+    images: []
+  }
+
+  getImages = () => {
+    API.getImages().then(img => {
+      this.setState(
+        {
+          images: img
+        }
+      )
+    })
+  }
+
+  componentDidMount() {
+    this.getImages();
+  }
+
   render() {
     return (
       <Router>

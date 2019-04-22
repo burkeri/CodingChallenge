2ae0322a5f23b8370fa7e8f0132f7344f19371da
Richelle Burke <burkeri@alumni.upenn.edu>
2019-04-22 01:38:58 -0400

    working on animations

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,13 @@ import NoMatch from "./pages/noMatch/NoMatch";
 
 // components
 import ImageCard from "./components/imageCard/ImageCard";
-import Image from "./components/Image/Image";
 
 class App extends Component {
   state = {
-    images: []
+    arr1: [],
+    arr2: [],
+    itemClass: "item",
+    hoverClass: "item hover"
   };
 
   componentDidMount() {
@@ -24,26 +26,100 @@ class App extends Component {
 
   getImages = () => {
     API.getImages().then(res => {
+      let arr1 = res.data.slice(0,9);
+      let arr2 = res.data.slice(9,18);
+      
       this.setState({
-        images: res.data
+        arr1: arr1,
+        arr2: arr2,
       });
+
     });
   };
 
+  // addClass = (e) => {
+  //   console.log(e.target);
+
+  // }
+
+  // removeClass = (e) => {
+  //   e.target.classList.remove("hover");
+  // }
+
+
+
+  // checkTopRow = () => {
+  //   for(let i=0; i <= (this.state.images.length/2); i++) {
+  //     if(!this.state.images[i].clicked) {
+  //       this.state.images[i] = true;
+  //       console.log("set true!");
+        
+  //     }
+  //     else {
+  //       this.state.images[i] = false;
+  //       console.log("set false!");
+
+  //     }
+  //   }
+  // }
+
+
+  /*
+  onmouseover loop through the first half of the images array
+  check if the one you are hovering over has a clicked attribute of false
+  if it does set it to true
+  now look through the entire half of that array again
+  check if any other elements have the clicked attribute set to true
+  if they do, set the clicked attribute to false
+  */
+
+
+
+   changeClicked = (name) => {
+      this.state.arr1.map(img => img.name === name ? img.clicked = true : img.clicked = false);
+      this.setState({ arr1: this.state.arr1 });
+    }
+    
+
+
+  // handleHover = (name, clicked, e) => {
+  //   this.changeClicked(name);
+  //   // this.checkTopRow(clicked);
+  // }
+
   render() {
+
+    let i = 0;
+    let j = 9;
+
     return (
       <Router>
         <Switch>
           {/* home render */}
           <Route
-            exact path="/"
-            render={() => (
+            exact
+            path="/"
+            render={() => [
               <div className="container">
-                {this.state.images.map(img => (
-                  <ImageCard />
+                {this.state.arr1.map(img => (
+                  <ImageCard
+                    name={img.name}
+                    changeClicked={this.changeClicked}
+                    clicked={img.clicked}
+                    id={`item${i++}`}
+                  />
+                ))}
+
+              </div>,
+              <div className="container">
+                {this.state.arr2.map(img => (
+                  <ImageCard 
+                    name={img.name}
+                    id={j++} 
+                  />
                 ))}
               </div>
-            )}
+            ]}
           />
 
           {/* 404 render */}
--- client/src/components/imageCard/ImageCard.js
+++ client/src/components/imageCard/ImageCard.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import "../../App.css";
 
 export class ImageCard extends Component {
+  state = {
+    itemClass: "item"
+  };
+
+  addClass = e => {
+    console.log(e.target);
+    e.target.classList.add("hover");
+  };
+  removeClass = e => {
+    e.target.classList.remove("hover");
+  };
+
   render() {
+
+    function test() { if (this.props.clicked) {console.log("hello!")} };
+
     return (
-      <div
-      >
-        
-      </div>
-    )
+      <div 
+        className={this.state.itemClass} 
+        // onMouseLeave={this.removeClass}
+        onMouseEnter={() => this.props.changeClicked(this.props.name)}
+        onMouseCapture={test()}
+        id={this.props.id}
+      />
+    );
   }
 }
 
-export default ImageCard
+export default ImageCard;
